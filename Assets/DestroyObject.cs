3db3853aa3437bf7e112ee1cd2eb4f3209f7c4ee
stevenshi19 <stevenwaseda87@gmail.com>
2020-08-02 23:32:40 +0900

    不要になったアイテムを順次破棄を実装しました。 発展課題は未実装です

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+
+    //Unity ちゃんのオブジェクト
+    private GameObject unitychan;
+
+    //Unity ちゃんとカメラの距離
+    private float difference=10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+    }
+
+    // Update is called once per frame
+
+    void Update(){
+
+        if (transform.position.z < (this.unitychan.transform.position.z - difference)){
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/MycameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MycameraController : MonoBehaviour {
+    //Unity ちゃんのオブジェクト
+    private GameObject unitychan;
+
+    //Unity ちゃんとカメラの距離
+    private float difference;
+
+	// Use this for initialization
+	void Start () {
+        
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+        //Unity ちゃんのカメラの位置(z座標)の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+
+	void Update () {
+        //Unity ちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+	}
+}
